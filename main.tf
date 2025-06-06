12cea94670e1ca540cf5ab6847ef99753ecab746
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2025-06-06 15:19:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,25 +12,25 @@ provider "aws" {
 }
 
 resource "aws_ebs_volume" "myvol1" {
-        availability_zone = "us-east-1a"
-        size              = 10
-        tags = {
-                 Name = "vol1"
-        }
+  availability_zone = "us-east-1a"
+  size              = var.size
+  tags = {
+    Name = "${var.tag}-volume"
+  }
 }
 
 resource "aws_instance" "instance1" {
-        ami = "ami-0953476d60561c955"
-        instance_type = "t2.small"
-        availability_zone = "us-east-1a"
-        key_name = "Swanand_test"
-        tags = {
-        Name = "Amazon Instance"
-}
+  ami               = var.ami
+  instance_type     = var.instance_type
+  availability_zone = "us-east-1a"
+  key_name          = "Swanand_test"
+  tags = {
+    Name = "${var.tag}-instance"
+  }
 }
 
 resource "aws_volume_attachment" "ebs_att_1" {
-        device_name = "/dev/sdh"
-        volume_id   = aws_ebs_volume.myvol1.id
-        instance_id = aws_instance.instance1.id
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.myvol1.id
+  instance_id = aws_instance.instance1.id
 }
